1e3a9b1c5e6a2f7aa32d9777e16ccc18b18647c6
karadagserhat <serhatkaradagg@outlook.com>
2024-01-15 22:21:58 +0300

    created new component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent, RouterOutlet],
+  imports: [
+    HeaderComponent,
+    NavbarComponent,
+    RouterOutlet,
+    FontAwesomeModule,
+    RouterLink,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  icon = faCoffee;
+}
