1e43c27fbb2c94c7feef3138c106f5b6f03255d0
karadagserhat <serhatkaradagg@outlook.com>
2024-01-17 12:14:35 +0300

    Created auth controllers

---
+++ backend/controllers/authController.js
@@ -0,0 +1,29 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+// const { attachCookiesToResponse, createTokenUser } = require("../utils");
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  // res.status(StatusCodes.CREATED).json({ user });
+};
+
+const login = async (req, res) => {
+  res.send("login oser");
+};
+
+const logout = async (req, res) => {
+  res.send("logout oser");
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
