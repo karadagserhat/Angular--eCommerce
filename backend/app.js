1f19714f5b3debd38bc1e540dd120d4c7db9c41f
karadagserhat <serhatkaradagg@outlook.com>
2024-01-17 12:12:28 +0300

    Created server

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const morgan = require("morgan");
+
+// database
+const connectDB = require("./db/connect");
+
+//  routers
+const authRouter = require("./routes/authRoutes");
+// const userRouter = require("./routes/userRoutes");
+// const productRouter = require('./routes/productRoutes');
+// const reviewRouter = require('./routes/reviewRoutes');
+// const orderRouter = require('./routes/orderRoutes');
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+// app.use('/api/v1/users', userRouter);
+// app.use('/api/v1/products', productRouter);
+// app.use('/api/v1/reviews', reviewRouter);
+// app.use('/api/v1/orders', orderRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
