f370e0c7d39b4513c5da1b3d589c635c0db26ae0
karadagserhat <serhatkaradagg@outlook.com>
2024-01-17 20:09:49 +0300

    Styled and new folder structure home page

---
+++ src/app/home/components/story/story.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-story',
+  standalone: true,
+  imports: [],
+  templateUrl: './story.component.html',
+  styleUrl: './story.components.css',
+})
+export class StoryComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,26 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { ServiceComponent } from './components/service/service.component';
+import { ProductsComponent } from './components/products/products.component';
+import { StoryComponent } from './components/story/story.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent, RouterOutlet, RouterLink],
+  imports: [
+    HeaderComponent,
+    NavbarComponent,
+    HeroComponent,
+    ServiceComponent,
+    ProductsComponent,
+    TestimonialComponent,
+    StoryComponent,
+    RouterOutlet,
+    RouterLink,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
