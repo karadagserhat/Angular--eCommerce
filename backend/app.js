ab2de58f7f3bdcf8ca67cf8177bf311a4b828380
karadagserhat <serhatkaradagg@outlook.com>
2024-01-18 21:09:36 +0300

    Improved review functionality

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const connectDB = require("./db/connect");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
-// const reviewRouter = require('./routes/reviewRoutes');
+const reviewRouter = require("./routes/reviewRoutes");
 // const orderRouter = require('./routes/orderRoutes');
 
 // middleware
@@ -37,7 +37,7 @@ app.use(fileUpload());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
-// app.use('/api/v1/reviews', reviewRouter);
+app.use("/api/v1/reviews", reviewRouter);
 // app.use('/api/v1/orders', orderRouter);
 
 app.use(notFoundMiddleware);
