2b8973dc2e49515e138920bed32b1480dbaaacd8
karadagserhat <serhatkaradagg@outlook.com>
2024-01-18 03:57:20 +0300

    authenticate and authorize user

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,14 @@ const express = require("express");
 const app = express();
 
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
 // database
 const connectDB = require("./db/connect");
 
 //  routers
 const authRouter = require("./routes/authRoutes");
-// const userRouter = require("./routes/userRoutes");
+const userRouter = require("./routes/userRoutes");
 // const productRouter = require('./routes/productRoutes');
 // const reviewRouter = require('./routes/reviewRoutes');
 // const orderRouter = require('./routes/orderRoutes');
@@ -22,9 +23,10 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use("/api/v1/auth", authRouter);
-// app.use('/api/v1/users', userRouter);
+app.use("/api/v1/users", userRouter);
 // app.use('/api/v1/products', productRouter);
 // app.use('/api/v1/reviews', reviewRouter);
 // app.use('/api/v1/orders', orderRouter);
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-// const { attachCookiesToResponse, createTokenUser } = require("../utils");
+const { attachCookiesToResponse, createTokenUser } = require("../utils");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -11,15 +11,48 @@ const register = async (req, res) => {
     throw new CustomError.BadRequestError("Email already exists");
   }
 
-  res.status(StatusCodes.CREATED).json({ email });
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ email, password, role });
+
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const login = async (req, res) => {
-  res.send("login oser");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
-  res.send("logout oser");
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000),
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
 
 module.exports = {
