2730f9f6dfaa20203b1f6f6b3885cf16d72fff20
karadagserhat <serhatkaradagg@outlook.com>
2024-01-18 21:21:02 +0300

    Implemented order functionality

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 // const rateLimiter = require('express-rate-limit');
@@ -21,13 +20,23 @@ const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
-// const orderRouter = require('./routes/orderRoutes');
+const orderRouter = require("./routes/orderRoutes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(morgan("tiny"));
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000,
+//     max: 60,
+//   })
+// );
+// app.use(helmet());
+// app.use(cors());
+// app.use(xss());
+// app.use(mongoSanitize());
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
@@ -38,7 +47,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/reviews", reviewRouter);
-// app.use('/api/v1/orders', orderRouter);
+app.use("/api/v1/orders", orderRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
